defb750e9be94789b98789d209c9b14eb0cfcb18
MatthewMosinski <91233770+mosinskimatt@users.noreply.github.com>
2022-09-26 21:16:43 -0400

    Add files via upload

---
+++ Week 3/MattsSportsEmporium/MattsSportsEmporium/Models/MattsSportsEmporiumModel.cs
@@ -0,0 +1,36 @@
+//using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace MattsSportsEmporium.Models
+{
+    public class MattsSportsEmporiumModel
+    {
+        [Required(ErrorMessage =
+            "The subtotal is required.")]
+        [Range(0.01, double.MaxValue, ErrorMessage = 
+            "Subtotal must at least .01.")]
+        public decimal? Subtotal { get; set; }
+
+        [Required(ErrorMessage =
+            "The tax percent is required.")]
+        [Range(0,10, ErrorMessage =
+            "Tax Percent must be a valid number from 0 to 10.")]
+        public decimal? TaxPercent { get; set; }
+  
+        
+        public decimal?  TotalCalculator()
+        {
+            
+            decimal? Total = (Subtotal * (TaxPercent / 100)) + Subtotal;
+            return Total;
+
+        }
+        public decimal? TaxCalculator()
+        {
+            
+            decimal? TaxAmount = Subtotal * (TaxPercent / 100);
+            return TaxAmount;
+        }
+
+    }
+}
