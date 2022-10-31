40cebe54ddee7fffa4074cd1487e289de3cabf92
MatthewMosinski <91233770+mosinskimatt@users.noreply.github.com>
2022-10-31 18:43:52 -0400

    Add files via upload

---
+++ Week 7/TipCalculatorWk7/TipCalculatorWk7/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using System.Security.Cryptography.Xml;
+using TipCalculatorWk7.Models;
+
+namespace TipCalculatorWk7.Controllers
+{
+   public class HomeController : Controller
+   {
+      private readonly ILogger<HomeController> _logger;
+
+      public HomeController(ILogger<HomeController> logger)
+      {
+         _logger = logger;
+      }
+
+      [HttpGet]
+      public IActionResult Index()
+      {
+         ViewBag.Fifteen = 0;
+         ViewBag.Twenty = 0;
+         ViewBag.TwentyFive = 0;
+         return View();                  
+
+      }
+
+      [HttpPost]
+      public IActionResult Index(TipCalculator model)
+      {
+         if (ModelState.IsValid)
+         {
+            ViewBag.Fifteen = model.Fifteen();
+            ViewBag.Twenty = model.Twenty();
+            ViewBag.TwentyFive = model.TwentyFive();
+         }
+         else
+         {
+            ViewBag.Fifteen = 0;
+            ViewBag.Twenty = 0;
+            ViewBag.TwentyFive = 0;
+         }
+         return View(model);
+      }
+   }
+}
---
+++ Week 7/TipCalculatorWk7/TipCalculatorWk7/Models/TipCalculator.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TipCalculatorWk7.Models
+{
+   public class TipCalculator
+   {
+      [Required(ErrorMessage = "Please enter a value for cost of meal.")]
+      [Range(0.0, 10000000.0, ErrorMessage = "Cost of meal must be greater than zero.")]
+      public double? MealCost { get; set; }
+
+        
+
+        public double? Fifteen()
+        {
+            double? Total = MealCost * (.15);
+            return Total;
+        }
+
+        public double? Twenty()
+        {
+            double? Total = MealCost * (.2);
+            return Total;
+        }
+
+        public double? TwentyFive()
+        {
+            double? Total = MealCost * (.25);
+            return Total;
+        }
+
+
+    }
+}
