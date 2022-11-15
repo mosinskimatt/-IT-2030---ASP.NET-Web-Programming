8df1d99858a46912996de8c9481f893a931fede6
MatthewMosinski <s01034789@acad.tri-c.edu>
2022-11-15 01:14:57 -0500

    repo update

---
+++ Week 3/MattsSportsEmporium/MattsSportsEmporium/Models/PriceCalculator.cs
@@ -0,0 +1,21 @@
+﻿namespace MattsSportsEmporium.Models
+{
+    public class PriceCalculator
+    {
+        public decimal SubTotal { get; set; }
+        public decimal TaxPercent { get; set; }
+        public decimal TaxAmount { get; set; }
+        public decimal Total { get; set; }
+
+        public void CalculateTotal()
+        {
+            
+            decimal _TaxAmount = System.Math.Round(SubTotal * TaxPercent / 100, 2);
+            decimal _Total = SubTotal + _TaxAmount;
+
+            TaxAmount = _TaxAmount;
+            Total = _Total;
+
+        }
+    }
+}
---
+++ Week 5/MattsSportsEmporium/MattsSportsEmporium/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using MattsSportsEmporium.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MattsSportsEmporium.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            ViewBag.Tot = 0;
+            ViewBag.Tax = 0;
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(decimal Subtotal, Decimal TaxPercent, MattsSportsEmporiumModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                ViewBag.Tot = model.TotalCalculator();
+                ViewBag.Tax = model.TaxCalculator();
+            }
+            else
+            {
+                ViewBag.Tot = 0;
+                ViewBag.Tax = 0;
+                
+            }
+            return View(model);
+        }
+    }
+}
